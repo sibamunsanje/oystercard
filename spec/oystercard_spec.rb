5053fb04c55d33c45a7682deaf260a23747dd505
mona lisa <sibaskye@gmail.com>
2019-11-11 17:24:36 +0000

    the failing test for in journey q8

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,6 +5,9 @@ describe Oystercard do
   it "has a balance of zero" do
     expect(subject.balance).to eq(0)
   end
+
+
+
   describe "top_up" do
     it {is_expected.to respond_to(:top_up).with(1).argument}
 
@@ -17,13 +20,25 @@ describe Oystercard do
       expect { subject.top_up(max_balance + 0.01) }.to raise_error("max balance #{max_balance} exceeded")
     end
   end
+
+
+
   describe "deduct" do
-    it {is_expected.to respond_to (:deduct).with(1).argument}
+    it {is_expected.to respond_to(:deduct).with(1).argument}
 
     it "can deduct the balance" do
       subject.top_up(10)
-      expect{subject.deduct 5}.to change{subject.balance}.by 5
+      expect {subject.deduct 5}.to change{subject.balance}.by -5
     end
+
+    end
+
+
+
+    describe "in_journey" do
+      it "is initially not in a journey" do
+        expect (subject).not_to be_in_journey
+      end
 
     end
 
